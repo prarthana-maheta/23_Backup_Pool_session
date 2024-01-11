4c932b99d25c0664ba397f876aef6294155371f7
prarthana.m <prarthana.m@simformsolutions.com>
2024-01-11 20:59:16 +0530

    latest code

---
+++ jan_11.py
@@ -0,0 +1,87 @@
+# # Collections in Python
+# """
+# Ordered     Immutable   string      "ahfahdxsd"
+# # Ordered     Mutable     list        [1,2,3]
+# # if for while
+# # Ordered     Immutable   tuple       (1,2,3)
+# # Unordered   Mutable     set         {1,2,3,5}
+# # Unordered   Mutable     Dict         {"name":"prarthana"}
+# #
+# # Two special collections:
+# # strings: Ordered & Immutable collections of characters              " "/ ' '
+# # dictionaries: Unordered & Mutable collections of key-value pairs
+# # """
+# #
+# # # # list: Ordered & Mutable collection of members
+# list1=[1,2,3,4,5,6,7,8,91,0]
+# print(type(list1))
+# print(len(list1))
+# print(list1[-1])
+#
+# #
+# # # numbers = [33,12, 0, -125, 44, 33, 4791234, -5592, 33]
+# # # # index     0  1    2     3   4
+# # # #          -9 -8    -7
+# # # # print(len(numbers))
+# # # # print(type(numbers))
+# # # # s='123456789'
+# # # # s=123456789
+# # # # print(list(s))
+# # # print(numbers[2:8])
+# # #
+# # print altrenate elements from list
+# #
+# # perform addition between first and last element
+# #
+# # reverse a list
+#
+#
+# # #
+# # #
+# # #
+# # #
+# # #
+# # #
+# # #
+# # #
+# # # print(len(numbers)-1)
+#
+# print(1+'1')
+# numbers = [12, 0, -125, 44, 33, 4791234, -5592]
+# # #
+# # print(sum(numbers))
+# # print(min(numbers))
+# # print(max(numbers))
+# print(sum(numbers[2:]))
+# print(min(numbers[1:5]))
+
+
+# i want sum from 3 element to last
+# we want min and max from 2 to 5 element
+
+# vegetables = ["carrot", "tomato", "potato", "spinach", "cucumber", "beetroot", "onion", "Lemon", 'Ice']
+# veg=['ice','ice','12','34']
+# veg[0]='ertyuihgfv'
+# print(int(veg[2])+int(veg[3]))
+# print(sum(vegetables))
+# can`t perform sum
+# print(min(veg))
+
+# str1="['12345        '' '' ']"
+# print(list(str1))
+
+# methods:
+
+list1=[1,2,3,'55','i','royal']
+# list1.append('1')
+# list1.append('2')
+# list1[0]='100'
+# list1.insert(0,'100')
+# list1.append('100')
+# list1.extend('abc')
+# add 67,89,60 to the list1 --list1.extend([67,89,60])
+# add '34' at 5 element --insert
+# replace 'royal ' with 'python' -- list1[-1]='python'
+# add 'royal' in list1  at last index --insert,append
+
+print(list1)
--- jan_4.py
+++ jan_4.py
@@ -1,55 +1,179 @@
-
-
-s1 = 'stDudents of this batch are going to rock the INDIAN software industry?'
-# print(s1.encode())
-# print(s1.encode("utf-16"))
-# print(s1.endswith("?"))
-# print(s1.endswith("try!"))
-# print(s1.endswith("is", 40))
-# s1.endswith("s", 40, 60)
-
-# print(s1.startswith("s"))
-# print(s1.startswith("t", 1,3))
+#
+#
+# s1 = 'stDudents of this batch are going to rock the INDIAN software industry?'
+#
+# # s2=s1.encode('utf-16')
+# # print(s2.decode('utf-16'))
+#
+# # print(s1.encode())
+# # print(s1.encode("utf-16"))
+#
+#
+# # print(s1.endswith("?"))
+# # print(s1.endswith("try!"))
+# # print(s1.endswith("is", 40))
+# # s1.endswith("s", 40, 60)
+# s1 = 'stuDdents of s23this batch are going to rock the INIDAN software industry?'
+# # print(s1.startswith("s23",10))
+# # print(s1.endswith(" ",10,56))
+#
+#
+# # print(s1.startswith("t", 1,3))
+# print(s1.find("D"))
+# print(s1.index("D"))
+# # print(s1.index("D",1,3))
+# print(s1.rfind(" "))
+# # print(s1.lfind("D"))
+# print(s1.rindex("D"))
+# # print(s1.lindex("D"))
+#
+# """
 # print(s1.find("D"))
 # print(s1.find("e"))
-# print(s1.index("D",1,3))
-print(s1.rfind("D",3))
-# print(s1.lfind("D"))
-print(s1.rindex("D"))
-# print(s1.lindex("D"))
+# print(s1.find("rock"))
+#
+# a = s1.find("e")
+# print(s1.find("e", a+1))
+#
+# print(s1.find("e", 5))
+# print(s1.find("e", 5, 30))
+#
+# print(s1.find("e", 5, -5))
+# print(s1.find("Python"))
+#
+# print()
+#
+# print(s1.index("D"))
+# print(s1.index("e"))
+# print(s1.index("rock"))
+#
+# a = s1.index("e")
+# print(s1.index("e", a+1))
+#
+# print(s1.index("e", 5))
+# print(s1.index("e", 5, 30))
+#
+# print(s1.index("e", 5, -5))
+# print(s1.index("Python"))
+#
+#
+# print(s1.rfind("e"))
+# print(s1.rfind("are", 20, -3))
+# print(s1.rindex("D"))
+# print(s1.rindex("r", 20, -20))
+# """
+#
+# # s1 = "Abc"
+# # # s2 = "5⁴"
+#
+# # # s5 = "二千二十二"
+# #
+# # # methods starting from is: All these methods return True or False
+# #
+# # # print(s1.isupper())
+# # # print(s1.islower())
+# # # print(s1.istitle())
+# #
+# s1 = "二千二十"
+# s2 = "AlakhPandya"
+# s3 = "9876543210"
+# s3 = "②⓪②②"
+# s4 = "½"
+# # s4 = "AlakhPandya9876543210"
+# print(s2.isnumeric())
+# print(s2.isalpha())
+# print(s4.isalnum())
+# print(s3.isdecimal())
+# print(s3.isdigit())
+# print(s1.isascii())
+# #
+# #
+# # # s5 = "₹5000"
+# s6 = "abc  fgh"
+# print(s6.isidentifier())
+#
+# #
+# s7 = " cghjkjhgfdcvb"
+# print(s7.isspace())
+# print(s7.isprintable())
+# #
+# # """
+# # The difference between isnumeric, isdigit & isdecimal
+# # """
+# # """
+# # print(s4.isdecimal())   # only recognizes digits from 0 to 9 and nothing else.
+# # print(s4.isdigit())     # also recognizes subscript, superscript, circled numbers
+# # print(s4.isnumeric())   # also recognizes roman numerals, vulgar fractions, digits from other languages
+# #
+# # print(s5.isnumeric())
+# # """
+# #
+# #
+# #
+# #
+# #
+# # # list(strings)
+# # # [] datatype list
 
-"""
-print(s1.find("D"))
-print(s1.find("e"))
-print(s1.find("rock"))
+s1 = 'stuDdents of s23this batch are going to rock the INIDAN software industry?'
 
-a = s1.find("e")
-print(s1.find("e", a+1))
+print('*'.join(s1.split()))
 
-print(s1.find("e", 5))
-print(s1.find("e", 5, 30))
+# # s6 = ('students of this batch aregoing to rock the indian software '
+# #       'industry!\nBecause they are very sincere.\nThey also do their homework on time.        ')
+# # # print(s6)
+# # # print(s6.split(" ")) #return list
+# # # print(s6.split("z"))
+# print(s6.split(" ", 4))
+# # #
+# # # print(s6.rsplit(" ",3))
+# # # print(s6.split(" "))
+# #
+# # # # print(s6)
+# # # print(s6.split("\n"))
+# # # print(s6.splitlines())
+# #
+# # # s7 = "Harsh is a good boy.
+# # # But, this sentence has an error."
+# #
+# # s8 = "a"
+# # s9 = s8.join("123")
+# # print(s9)
+# #
+# # # print(" ".join(s8))
+# #
+s6 = 'sssssa       students         of this batch are going batch are to rock the indian software industry!         '
+print(s6.partition("batch"))
+# # # print(s6.partition("are are")) #return tuple
+# # # print(s6.rpartition("batch"))
+# #
+print(s6.strip("s"))
+s8 = "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$Happy$$Birthday!$$$$$$$$$$$$$$$$$"
+# #
+print(s8.replace("$", " "))
+#
+# """
+#
+# s8 = "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$Happy$$Birthday!$$$$$$$$$$$$$$$$$"
+# print(s8.lstrip("$"))
+# print(s8.rstrip("$"))
+# print(s8.strip("$"))
+# """
+#
 
-print(s1.find("e", 5, -5))
-print(s1.find("Python"))
-
-print()
-
-print(s1.index("D"))
-print(s1.index("e"))
-print(s1.index("rock"))
-
-a = s1.index("e")
-print(s1.index("e", a+1))
-
-print(s1.index("e", 5))
-print(s1.index("e", 5, 30))
-
-print(s1.index("e", 5, -5))
-print(s1.index("Python"))
 
 
-print(s1.rfind("e"))
-print(s1.rfind("are", 20, -3))
-print(s1.rindex("D"))
-print(s1.rindex("r", 20, -20))
-"""
+encode()
+decode()
+endswith
+startswith
+split
+partition
+strip
+index
+find
+rindex
+rfind
+replace
+
+
